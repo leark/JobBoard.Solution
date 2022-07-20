df297306bc6b4ed4e05f1fd08b10566d8023c984
Seung Lee <shl7@uw.edu>
2022-07-20 09:49:37 -0700

    Add initial files

---
+++ JobBoard.Tests/Model.Tests/JobOpening.Tests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using JobBoard.Models;
+using System;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+
+    // [TestMethod]
+    // public void ItemConstructor_CreatesInstanceOfItem_Item()
+    // {
+    //   Item newItem = new Item("test");
+    //   Assert.AreEqual(typeof(Item), newItem.GetType());
+    // }
+
+  }
+}
---
+++ JobBoard/Controllers/JobOpeningsControllers.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using JobBoard.Models;
+using System.Collections.Generic;
+
+namespace JobBoard.Controllers
+{
+  public class JobOpeningsController : Controller
+  {
+
+
+  }
+}
